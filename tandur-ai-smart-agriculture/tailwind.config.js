db92d481eaba51ef9a0a845a733f418dc1d0fb93
Yudi Firmansyah <iduyfirmansyah27@gmail.com>
2025-11-03 20:20:06 +0700

    Tandur

--- tandur-ai-smart-agriculture/tailwind.config.js
+++ tandur-ai-smart-agriculture/tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -16,4 +16,4 @@ module.exports = {
     },
   },
   plugins: [],
-};
+}
